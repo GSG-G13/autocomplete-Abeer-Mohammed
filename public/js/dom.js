6e8131df20e1f070f3ee904f269dd8cbe6110d9c
abeerAyad <abeerayyad2000@gmail.com>
2023-03-22 11:21:38 +0200

    handle fun Relates#26

---
+++ public/js/dom.js
@@ -0,0 +1,31 @@
+const createElement = (tagName,className,parent) => {
+    const element = document.createElement(tagName);
+    element.className = className;
+    parent.appendChild(element);
+    return element;
+}
+
+
+const renderWords = (words) => {
+  const listData = document.querySelector("ul");
+  console.log(listData)
+    words.forEach(word => {
+        const list = createElement("li","list-word",listData);
+        list.textContent =word;
+    });
+
+}
+
+const wordInput = document.querySelector("input");
+wordInput.addEventListener("keyup", (e) => {
+    const value = e.target.value;
+   get('/api/word/filtered',(data) => {
+    console.log(data)
+    
+    document.querySelectorAll('li').forEach((item) => {
+      item.remove();
+    });
+    renderWords(data);
+    console.log(document.querySelectorAll('li'))
+  });
+})
