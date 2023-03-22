551573029d0259e978371869d3f8f7d42badbc1a
Mohammed Mahani <mohammed.mahanu@gmail.com>
2023-03-22 10:34:40 +0200

    add handler Relates #12

---
+++ src/handler/handlerPublic.js
@@ -0,0 +1,42 @@
+const fs = require("fs");
+const path = require("path");
+
+let contentType = {
+  ".html": "text/html",
+  ".css": "text/css",
+  ".txt": "text/plain",
+  ".jpg": "text/jpeg",
+  ".js": "text/javascript",
+  ".json": "application/json",
+  ".ico": "text/plain",
+};
+
+const handlePublic = (url, response) => {
+  const pathFile = path.join(__dirname, "..", "..", "public", url);
+  const extension = path.extname(pathFile);
+
+  fs.readFile(pathFile, "utf8", (err, file) => {
+    if (err) {
+      response.writeHead(500, { "Content-Type": "text/plain" });
+      response.end("Server Error!");
+    } else {
+      response.writeHead(200, { "Content-Type": contentType[extension] });
+      response.end(file);
+    }
+  });
+};
+
+const handlePublicHome = (url, response) => {
+  const pathFile = path.join(__dirname, "..", "..", "src", url);
+  const extension = path.extname(pathFile);
+  console.log(extension);
+  fs.readFile(pathFile, "utf8", (err, file) => {
+    if (err) {
+      response.writeHead(500, contentType["plain"]);
+      response.end("Server Error!");
+    } else {
+      response.writeHead(200, contentType[extension]);
+      response.end(file);
+    }
+  });
+};
